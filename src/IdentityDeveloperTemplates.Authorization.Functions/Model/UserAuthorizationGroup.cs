d43ecf9560728e3817630d6a32b01a940a05b7ac
Daniel <krzyczkowski.daniel@op.pl>
2020-07-27 14:36:04 +0200

    Add json properties to the user authorization group class

--- src/IdentityDeveloperTemplates.Authorization.Functions/Model/UserAuthorizationGroup.cs
+++ src/IdentityDeveloperTemplates.Authorization.Functions/Model/UserAuthorizationGroup.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace IdentityDeveloperTemplates.Authorization.Functions.Model
 {
     public class UserAuthorizationGroup
     {
+        [JsonProperty("id")]
         public Guid Id { get; set; }
+        [JsonProperty("userId")]
         public Guid UserId { get; set; }
+        [JsonProperty("groupId")]
         public Guid GroupId { get; set; }
     }
 }
